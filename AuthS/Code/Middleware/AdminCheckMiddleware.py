da1bb31fc51f07723b1f2245ab982f9e41584bb9
Harsh <66474347+ViK-Dev1@users.noreply.github.com>
2024-11-09 17:03:02 +0100

    AuthS - middleware
    Added 2 more middlewares to check if an admin is calling the service and to manage validation errors of the request

---
+++ AuthS/Code/Middleware/AdminCheckMiddleware.py
@@ -0,0 +1,51 @@
+from fastapi import HTTPException, Response, status
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+from starlette.types import ASGIApp
+import jwt
+from jwt import ExpiredSignatureError, InvalidSignatureError
+from Config.appsettings import SKJWT_SECRETKEY_JWT
+from BL.CommonFun import IsNullOrEmpyStr
+
+# ICAuthCheckMiddleware
+
+class AdminCheckMiddleware(BaseHTTPMiddleware):
+    def __init__(self, app: ASGIApp, protectedRoutes: list):
+        super().__init__(app)
+        self.protectedRoutes = protectedRoutes
+
+    async def dispatch(self, request: Request, call_next):
+        # Protected routes list
+        protectedRoutesList = self.protectedRoutes
+        if(self.protectedRoutes is None):
+            self.protectedRoutes == []
+        
+        # Get the current route
+        current_route = request.scope["path"]
+        if(len(current_route)>1):
+            current_route = current_route[1:]
+
+        # Check if the route is in our protected routes
+        if (current_route in protectedRoutesList):
+
+            # Extract the username from the token
+            auth_header = request.headers.get("Authorization")
+            token = auth_header.split(" ")[1]
+
+            if IsNullOrEmpyStr(SKJWT_SECRETKEY_JWT):
+                raise RuntimeError('CONFIG KEY NOT FOUND: SKJWT')
+            sks = SKJWT_SECRETKEY_JWT
+            try:
+                payload = jwt.decode(token, sks, algorithms=["HS256"])
+                username = payload["sub"].get('username')
+                #check if the user is an admin (if he has admin in its name) // used just here
+                if(username == None
+                   or username == ''
+                   or 'admin' not in username):
+                    return Response(status_code=status.HTTP_403_FORBIDDEN)
+
+            except (Exception, ExpiredSignatureError, InvalidSignatureError) as decodeExcp:
+                return Response(status_code=status.HTTP_403_FORBIDDEN)
+        
+        # If not a protected route, skip to the next code section
+        return await call_next(request)
---
+++ AuthS/Code/Middleware/ErrorHandlerMiddleware.py
@@ -0,0 +1,20 @@
+from argparse import ArgumentError
+import anyio
+from fastapi import Request, Response, status
+from starlette.middleware.base import BaseHTTPMiddleware
+
+from BL.CommonFun import CreateErrorResponse
+
+# ErrorHandlerMiddleware
+
+class ErrorHandlerMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        try:
+            response = await call_next(request)
+            return response
+        except (Exception, RuntimeError) as e:
+            if(isinstance(e,AttributeError) or 
+               (e.detail != None and 'out of range' in e.detail)):
+                return Response(status_code=status.HTTP_400_BAD_REQUEST)
+            else:
+                return CreateErrorResponse(status.HTTP_500_INTERNAL_SERVER_ERROR, 'ERR-MDLWR')
