70778740259b1576f67430a039217e5df03e6d9e
Harsh <66474347+ViK-Dev1@users.noreply.github.com>
2024-11-09 17:09:34 +0100

    AuthS - protected routes
    Defined all the protected routes by the middlewares

--- AuthS/Code/BL/ProtectedRoutes.py
+++ AuthS/Code/BL/ProtectedRoutes.py
@@ -2,12 +2,19 @@
 class ProtectedRoutes:
     # routes that require authentication
     authProtectedRoutes = [
+        'ICReactivateUsr',
         'ICChangeUsr',
         'ICChangeEmail'
     ]
 
     # routes for internal communication
     icProtectedRoutes = [
+        'ICReactivateUsr',
         'ICChangeUsr',
         'ICChangeEmail'
+    ]
+
+    # routes reserved for super user / admin
+    adminProtectedRoutes = [
+        'ICReactivateUsr'
     ]
