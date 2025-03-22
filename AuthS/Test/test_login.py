aabc426c25986e0e04912b7e43aee886035125f5
Harsh <66474347+ViK-Dev1@users.noreply.github.com>
2025-03-22 09:39:11 +0100

    AuthS - testing
    Creazione test per gli endpoint per
    - controllare lo stato del servizio
    - controllare il servizio di login

---
+++ AuthS/Test/test_login.py
@@ -0,0 +1,157 @@
+'''
+ TEST - Login
+ Test per verificare il corretto funzionamento del servizio di login
+'''
+import requests
+
+ABU_API_BASE_URL = 'http://127.0.0.1:8001/'
+
+def test_login_Campi1():
+    '''
+    login test - Campo email mancante
+    '''
+    url = ABU_API_BASE_URL + 'login'
+    data = {
+        'email': '', 
+        'password': 'passwordSicura' 
+    }
+    response = requests.post(url, json=data)
+    responseErrMsg = response.json()['detail']['field']
+    assert response.status_code == 400
+    assert "email" in responseErrMsg
+def test_login_Campi2():
+    '''
+    login test - Campo password mancante
+    '''
+    url = ABU_API_BASE_URL + 'login'
+    data = {
+        'email': 'testUser12', 
+        'password': '' 
+    }
+    response = requests.post(url, json=data)
+    responseErrMsg = response.json()['detail']['field']
+    assert response.status_code == 400
+    assert "password" in responseErrMsg
+
+def test_login_NonRegistrato():
+    '''
+    login test - Utente non registrato
+    '''
+    url = ABU_API_BASE_URL + 'login'
+    data = {
+        'email': 'utenteNonEsistente', 
+        'password': 'passwordSicura' 
+    }
+    response = requests.post(url, json=data)
+    responseErrMsg = response.json()['detail']
+    assert response.status_code == 400
+    assert "non registrata" in responseErrMsg
+# test sull'utente con username: testUser1
+def test_login_PwdSbagliata():
+    '''
+    login test - Utente registrato ma credenziali errate
+    Consiglio - per testare bene questo caso di test, rimuovere il record dalla 
+    tabella dei tentativi di accesso e riattivare l'utenza di test
+    '''
+    url = ABU_API_BASE_URL + 'login'
+    data = {
+        'email': 'testUser1', 
+        'password': 'passwordSicura' 
+    }
+    response = requests.post(url, json=data)
+    responseErrMsg = response.json()['detail']
+    assert response.status_code == 400
+    assert "sono errate" in responseErrMsg or "numerosi tentativi" in responseErrMsg
+def test_login_TroppiAccessi():
+    '''
+    login test 3 - L'utente ha effettuato troppi accessi
+    '''
+    url = ABU_API_BASE_URL + 'login'
+    data = {
+        'email': 'testUser1', 
+        'password': 'passwordSicura' 
+    }
+    requests.post(url, json=data) #secondo tentativo
+    requests.post(url, json=data) #terzo tentativo
+    requests.post(url, json=data) #quarto tentativo
+    response = requests.post(url, json=data)
+    responseErrMsg = response.json()['detail']
+    assert response.status_code == 400
+    assert "numerosi tentativi" in responseErrMsg
+def test_login_UtenteDisabilitatoPwd():
+    '''
+    login test - Login corretto, ma l'utenza risulta disabilitata
+    a causa di un numero eccessivo di accessi con pwd sbagliate
+    '''
+    url = ABU_API_BASE_URL + 'login'
+    data = {
+        'email': 'testUser1', 
+        'password': 'funziona123' 
+    }
+    response = requests.post(url, json=data)
+    responseErrMsg = response.json()['detail']
+    assert response.status_code == 401
+# test sull'utente con username: testUser2
+def test_login_UtenteDisabilitato():
+    '''
+    login test - Login corretto, ma l'utenza risulta disabilitata da 
+    un utente con privilegi alti
+    '''
+    url = ABU_API_BASE_URL + 'login'
+    data = {
+        'email': 'testUser2', 
+        'password': 'funziona123' 
+    }
+    response = requests.post(url, json=data)
+    responseErrMsg = response.json()['detail']
+    assert response.status_code == 401
+# test sull'utente con username: testUser3
+def test_login_OK_PwdScadutaBool():
+    '''
+    login test - Login corretto, ma l'utenza ha la pwd scaduta
+    e quindi riceverà nelle azioni obbligatorie di effettuare il cambio pwd
+    '''
+    url = ABU_API_BASE_URL + 'login'
+    data = {
+        'email': 'testUser3', 
+        'password': 'funziona123' 
+    }
+    response = requests.post(url, json=data)
+    response_json = response.json()
+    assert response.status_code == 200
+    assert len(response_json['token']) > 10
+    assert 'CHGPWD' in response_json['requiredActions']
+# test sull'utente con username: testUser4
+def test_login_OK_PwdScadutaDT():
+    '''
+    login test - Login corretto, ma l'utenza ha la pwd scaduta
+    perchè la data di ultima modifica della password supera il
+    limite entro cui bisogna cambiarla
+    '''
+    url = ABU_API_BASE_URL + 'login'
+    data = {
+        'email': 'testUser4', 
+        'password': 'funziona123' 
+    }
+    response = requests.post(url, json=data)
+    response_json = response.json()
+    assert response.status_code == 200
+    assert len(response_json['token']) > 10
+    assert 'CHGPWD' in response_json['requiredActions']
+# test sull'utente con username: harsh-admin
+def test_login_OK():
+    '''
+    login test - Login corretto, ma l'utenza risulta disabilitata da 
+    un utente con privilegi alti
+    '''
+    url = ABU_API_BASE_URL + 'login'
+    data = {
+        'email': 'harsh-admin', 
+        'password': 'funziona123!' 
+    }
+    response = requests.post(url, json=data)
+    response_json = response.json()
+    assert response.status_code == 200
+    assert response_json['userData'] != None
+    assert len(response_json['token']) > 10
+    assert len(response_json['requiredActions']) == 0
---
+++ AuthS/Test/test_service_status.py
@@ -0,0 +1,23 @@
+'''
+ TEST - Service Status
+ Test per verificare il corretto stato di attività del servizio
+'''
+import requests
+
+ABU_API_BASE_URL = 'http://127.0.0.1:8001/'
+
+def test_checkS_1():
+    '''
+    checkS test1 - controlla se il servizio è raggiungibile    
+    '''
+    url = ABU_API_BASE_URL + 'checkS'
+    response = requests.get(url)
+    assert response.status_code == 200
+
+def test_checkSDB_2():
+    '''
+    checkSDB test2 - controlla se il db del servizio è raggiungibile
+    '''
+    url = ABU_API_BASE_URL + 'checkSDB'
+    response = requests.get(url)
+    assert response.status_code == 200
